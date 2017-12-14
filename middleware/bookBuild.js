d5afd4a62a1047437cfd82d2e2cf7d0e84d12eaf
Matheus <matheus510.fonseca@gmail.com>
2017-12-14 17:10:31 -0200

    migrating repositories

--- middleware/bookBuild.js
+++ middleware/bookBuild.js
@@ -1,34 +1,4 @@
 module.exports = function(req, res, next) {
-/*     const containers = res.locals.data.conteudo[0].ArvoreDeOrdenacao
-    const noticias = containers.map((container) => container.items)
-    
-    const blocosDeNoticia = noticias.reduce((acc, blocoDeNoticias, index) => {
-        acc = acc || []
-        let bloquinho = blocoDeNoticias.map((noticia) => {
-            
-            return noticia = {
-                id: noticia.IdNoticia,
-                fonte: noticia.Fonte,
-                titulo: noticia.TituloNoticia
-            }
-        })
-        acc.push(bloquinho)
-        return acc
-    
-    }, [])
-    res.locals.blocosDeNoticia = blocosDeNoticia */
+
     next()
 }
-/* im a total failure */
-/* 
-TRANSPILE templates.
-to pug. 
-*/
-
-/*
-Prepare URLs before passing them into pug templates.
-do a service for this?
-*/ 
-
-//antes de colocar as noticias no book, confere se é fonte restrita.
-//template.confereFonteRestrita(noticias[$index].FonteMaxpress.Id, noticias[$index].IdNoticia)
